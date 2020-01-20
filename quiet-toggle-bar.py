0ac62ff2f56eb84bfb2ca7d3e0358a88b8b666fe
Tom Crossley FEARNLEY <tcac2@cam.ac.uk>
2020-01-20 22:17:19 +0000

    even lazier approach: just run both commands as it doesn't matter if one fails

--- quiet-toggle-bar.py
+++ quiet-toggle-bar.py
@@ -8,21 +8,13 @@ import asyncio
 async def main():
     def on_event(self, e):
         ws = 99
-        if instance == 'polybar':
-            if e.current.num == ws:
-                run(['polybar-msg', 'cmd', 'hide'])
-            elif e.old and e.old.num == ws:
-                run(['polybar-msg', 'cmd', 'show'])
-        elif instance == 'i3bar':
-            if e.current.num == ws:
-                run(['i3-msg', 'bar mode', 'invisible'])
-            elif e.old and e.old.num == ws:
-                run(['i3-msg', 'bar mode', 'dock'])
+        if e.current.num == ws:
+            run('i3-msg bar mode invisible'.split())
+            run('polybar-msg cmd hide'.split())
+        elif e.old and e.old.num == ws:
+            run('i3-msg bar mode dock'.split())
+            run('polybar-msg cmd show'.split())
 
-    if 'i3bar' in str(check_output('ps -A | grep i3bar', shell=True)):
-        instance = 'i3bar'
-    elif 'process' in str(check_output('ps -A | grep process', shell=True)):
-        instance = 'polybar'
     c = await Connection(auto_reconnect=True).connect()
     workspaces = await c.get_workspaces()
     c.on(Event.WORKSPACE_FOCUS, on_event)
